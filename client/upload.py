65b246ed4e359787d2f9ccfb9bca8125b5f3463a
MATTEO <mattemendu@gmail.com>
2023-04-30 16:51:19 -0700

    exp1 cleaning

--- client/upload.py
+++ client/upload.py
@@ -17,17 +17,16 @@ img_base64 = base64.b64encode(img_data).decode('utf-8')
 
 # Make a POST request with the base64-encoded image string as the request body
 url = "http://localhost:8000/img_object_classification"
-# headers = {"Content-type": "text/plain"}
+
 headers = {"Content-type": "application/json"}
 
 data = {
-    "name": "John Doe",
-    "age": 30,
+    "type_task": "IMAGE_CLASS",
     "image": img_base64
 }
 json_data = json.dumps(data)
 
 response = requests.post(url, data=json_data, headers=headers)
 
-# Print the response from the server
+
 print(response.text)
